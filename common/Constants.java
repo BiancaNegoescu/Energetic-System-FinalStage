40e7ed6161b8774ed64691466f88bd34ef16d0c2
BiancaNegoescu <72906162+BiancaNegoescu@users.noreply.github.com>
2021-01-24 03:51:23 +0200

    Added Constants package

---
+++ common/Constants.java
@@ -0,0 +1,38 @@
+package common;
+
+public final class Constants {
+    private Constants() {
+    }
+
+    public static final String NUMBEROFTURNS = "numberOfTurns";
+    public static final String INITIALDATA = "initialData";
+    public static final String CONSUMERS = "consumers";
+    public static final String CONSUMER = "consumer";
+    public static final String ID = "id";
+    public static final String INITIALBUDGET = "initialBudget";
+    public static final String MONTHLYINCOME = "monthlyIncome";
+    public static final String DISTRIBUTORS = "distributors";
+    public static final String DISTRIBUTOR = "distributor";
+    public static final String CONTRACTLENGTH = "contractLength";
+    public static final String INITIALINFRASTRUCTURECOST = "initialInfrastructureCost";
+    public static final String INITIALPRODUCTIONCOST = "initialProductionCost";
+    public static final String MONTHLYUPDATES = "monthlyUpdates";
+    public static final String NEWCONSUMERS = "newConsumers";
+    public static final String DISTRIBUTORCHANGES = "distributorChanges";
+    public static final String PRODUCERCHANGES = "producerChanges";
+    public static final String INFRASTRUCTURECOST = "infrastructureCost";
+    public static final String PRODUCTIONCOST = "productionCost";
+    public static final String CONTRACTS = "contracts";
+    public static final String PRICE = "price";
+    public static final String REMAINED_MONTHS = "remainedContractMonths";
+    public static final String PRODUCERS = "producers";
+    public static final String ENERGY_TYPE = "energyType";
+    public static final String MAXDISTRIBUTORS = "maxDistributors";
+    public static final String PRICE_KW = "priceKW";
+    public static final String ENERGYPERDISTRIBUTOR = "energyPerDistributor";
+    public static final String ENERGYNEEDED_KW = "energyNeededKW";
+    public static final String PRODUCERSTRATEGY = "producerStrategy";
+
+
+    // add any constants you think you may use
+}
